a6069027803454e224ca7fa95ac6427c3bb8e1d3
Ayodeji <olusanyasamson633@gmail.com>
2024-02-06 12:41:01 +0100

    validate-userOperation

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
 import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
 import {UserOperation} from "@account-abstraction/contracts/interfaces/UserOperation.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Account is IAccount {
     uint256 public count;
@@ -12,12 +13,13 @@ contract Account is IAccount {
         i_owner = owner;
     }
 
-    function validateUserOp(UserOperation calldata userOp, bytes32, uint256)
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256)
         external
-        pure
+        view
         returns (uint256 validationData)
     {
-        return 0;
+        address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(userOpHash), userOp.signature);
+        return i_owner == recovered ? 0 : 1;
     }
 
     function execute() external {
--- scripts/execute.js
+++ scripts/execute.js
@@ -1,13 +1,13 @@
 const hre = require("hardhat");
 
 const FACTORY_NOUNCE = 1;
-const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const EP_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const PM_ADDR = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 async function main() {
-  const [signers0] = await hre.ethers.getSigners();
-  const address0 = await signers0.getAddress();
+  const [signer0, signer1] = await hre.ethers.getSigners();
+  const address0 = await signer0.getAddress();
 
   const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDR);
 
@@ -43,14 +43,17 @@ async function main() {
     nonce: await entryPoint.getNonce(sender, 0),
     initCode,
     callData,
-    callGasLimit: 200_000,
-    verificationGasLimit: 200_000,
+    callGasLimit: 400_000,
+    verificationGasLimit: 400_000,
     preVerificationGas: 50_000,
     maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
     maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
     paymasterAndData: PM_ADDR,
     signature: "0x",
   };
+
+  const userOpHash = await entryPoint.getUserOpHash(userOp);
+  userOp.signature = signer0.signMessage(hre.ethers.getBytes(userOpHash));
 
   const tx = await entryPoint.handleOps([userOp], address0);
   const receipt = await tx.wait();
